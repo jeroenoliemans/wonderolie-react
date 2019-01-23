6953d5d8d8c57ed29579df28bd70256fa5302df3
Jeroen Oliemans <joliemans@acsi.eu>
2019-01-23 22:22:16 +0100

    added boids

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router} from 'react-router-dom';
+import BoidSimulation from './boids/BoidSimulation';
+
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
+
+window.setTimeout(() => {
+    // initialize boids
+    const boidSimulation = new BoidSimulation();
+    boidSimulation.startBoidSimulation();  
+}, 300)
+
+
