f7ce9117a1286c5923a6cc85140ff150f7db63f3
Jeroen Oliemans <joliemans@acsi.eu>
2019-03-03 17:53:42 +0100

    added footer styling

--- src/components/PortFolioFooter/PortFolioFooter.js
+++ src/components/PortFolioFooter/PortFolioFooter.js
@@ -1,14 +1,21 @@
 import React, {component} from 'react';
-import './PortFolioFooter.css'
+import './PortFolioFooter.css';
+import { FiLinkedin, FiGithub } from 'react-icons/fi';
 
 const PortFolioFooter = () => {
     const getCurrentYear = () => ((new Date()).getFullYear());
 
     return (
         <div className="PortFolioFooter">
-          <a href="https://www.linkedin.com/in/jeroenoliemans/" title="LinkedIn Jeroen Oliemans">LinkedIn</a>
-          <a href="https://github.com/jeroenoliemans" title="Github Jeroen Oliemans">Github</a>
-          <span>Copyright wonderolie {getCurrentYear()}</span>
+          <a className="PortFolioFooterLink" href="https://www.linkedin.com/in/jeroenoliemans/" title="LinkedIn Jeroen Oliemans">
+            <FiLinkedin className="PortFolioFooterIcon"/> 
+            <span className="PortFolioFooterLinkText">LinkedIn</span>
+          </a>
+          <a className="PortFolioFooterLink" href="https://github.com/jeroenoliemans" title="Github Jeroen Oliemans">
+            <FiGithub className="PortFolioFooterIcon"/>
+            <span className="PortFolioFooterLinkText">Github</span>
+            </a>
+          <span className="PortFolioFooterText">Copyright wonderolie {getCurrentYear()}</span>
         </div>
       );
 }
